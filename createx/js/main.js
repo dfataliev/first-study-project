f0549ec55514667ec824fc4656a701822d3d8cec
old-mechanic <knts97@ya.ru>
2023-12-28 10:57:32 +0500

    finished third section

--- createx/js/main.js
+++ createx/js/main.js
@@ -5,4 +5,10 @@ Fancybox.bind("[data-fancybox]", {
 $(function () {
   var mixer = mixitup('.directions__list');
 
+ $('.directions__filter-btn').on('click', function() {
+  $('.directions__filter-btn').removeClass('directions__filter-btn--active')
+  $(this).addClass('directions__filter-btn--active')
+
+ })
+  
 })
