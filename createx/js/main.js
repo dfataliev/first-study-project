311cea9f9a5b9ee072c55f82388c4274612b36c9
dfataliev <knts97@ya.ru>
2023-12-29 21:00:02 +0500

    merge

--- createx/js/main.js
+++ createx/js/main.js
@@ -1,14 +1,36 @@
 Fancybox.bind("[data-fancybox]", {
-    // Your custom options
-  });
+  // Your custom options
+});
 
 $(function () {
-  var mixer = mixitup('.directions__list');
+  var mixer = mixitup(".directions__list");
 
- $('.directions__filter-btn').on('click', function() {
-  $('.directions__filter-btn').removeClass('directions__filter-btn--active')
-  $(this).addClass('directions__filter-btn--active')
+  $(".directions__filter-btn").on("click", function () {
+    $(".directions__filter-btn").removeClass("directions__filter-btn--active");
+    $(this).addClass("directions__filter-btn--active");
+  });
 
- })
-  
-})
+  $('.team__slider').slick({
+    arrows: false,
+    slidesToShow: 4,
+    infinite: true,
+    draggable: false,
+    waitForAnimate: false,
+    dots: true,
+    dotsClass: 'slick-dots',
+   
+  })    
+
+    $('.team__slider-prev').on('click', function (e){
+      e.preventDefault()
+      $('.team__slider').slick('slickPrev')
+    })
+
+    $('.team__slider-next').on('click', function (e){
+      e.preventDefault()
+      $('.team__slider').slick('slickNext')
+    })
+
+
+
+});
