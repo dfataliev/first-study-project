b6933f1989c962b2927961a11488c26a0e89a487
dfataliev <knts97@ya.ru>
2023-12-30 16:21:43 +0500

    slider finished

--- createx/js/main.js
+++ createx/js/main.js
@@ -10,24 +10,43 @@ $(function () {
     $(this).addClass("directions__filter-btn--active");
   });
 
-  $('.team__slider').slick({
+  $(".team__slider").slick({
     arrows: false,
     slidesToShow: 4,
     infinite: true,
     draggable: false,
     waitForAnimate: false,
-   
-  })    
+  });
 
-    $('.team__slider-prev').on('click', function (e){
-      e.preventDefault()
-      $('.team__slider').slick('slickPrev')
-    })
+  $(".team__slider-prev").on("click", function (e) {
+    e.preventDefault();
+    $(".team__slider").slick("slickPrev");
+  });
 
-    $('.team__slider-next').on('click', function (e){
-      e.preventDefault()
-      $('.team__slider').slick('slickNext')
-    })
+  $(".team__slider-next").on("click", function (e) {
+    e.preventDefault();
+    $(".team__slider").slick("slickNext");
+  });
+
+
+$(".testimonials__slider").slick({
+
+arrows: false,
+dots: true,
+appendDots: $(".testimonials__dots"),
+
+})
+
+
+$(".testimonials__prev").on("click", function (e) {
+  e.preventDefault();
+  $(".testimonials__slider").slick("slickPrev");
+});
+
+$(".testimonials__next").on("click", function (e) {
+  e.preventDefault();
+  $(".testimonials__slider").slick("slickNext");
+});
 
 
 
